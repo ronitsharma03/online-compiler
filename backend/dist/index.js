e81cd80979e3647d7cf5eeb242c3e83f639b76a4
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-08-14 17:37:17 +0530

    vercel.json update

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,7 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = require("dotenv");
+const db_1 = require("./lib/db");
+const compilerRouter_1 = require("./routes/compilerRouter");
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+(0, dotenv_1.config)();
+(0, db_1.dbConnect)();
+app.use("/compiler", compilerRouter_1.compilerRouter);
 app.get("/", (req, res) => {
     res.status(200).send("Health is good");
 });
